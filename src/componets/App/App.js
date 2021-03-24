05341fbc05282ed266da70ebe3bd57807363dd0c
Shigatsi <shigatsii@gmail.com>
2021-03-24 19:25:41 +0300

    feat: all routes added

--- src/componets/App/App.js
+++ src/componets/App/App.js
@@ -5,15 +5,36 @@ import './App.css';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer'
 
 function App() {
   return (
     <div className="page">
-      'THIS SHIT IS WORKING!!!'
-      <Header/>
-      <Route path = '/'>
-        <Main/>
-      </Route>
+      <Switch>
+        <Route exact path = '/'>
+          <Header />
+          <Main />
+          <Footer />
+        </Route>
+        <Route path = '/movies'>
+            movies
+        </Route>
+        <Route path = '/saved-movies'>
+          saved-movies
+        </Route>
+        <Route path = '/profile'>
+          profile
+        </Route>
+        <Route path = '/sign-up'>
+          sign-up
+        </Route>
+        <Route path = '/sign-in'>
+          sign-in
+        </Route>
+        <Route path = '*'>
+          NOT_FOUND!
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/componets/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer () {
+  return (
+    <div className = 'footer'>
+     Учебный проект Яндекс.Практикум х BeatFilm.
+    </div>
+  );
+}
+
+export default Footer;
