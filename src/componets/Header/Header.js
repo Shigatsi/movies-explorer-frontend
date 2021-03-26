47c79199664c9c9d615156359519ff9572eedeb7
Shigatsi <shigatsii@gmail.com>
2021-03-26 19:28:25 +0300

    feat: default header added

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -3,11 +3,21 @@ import { Route, Link } from 'react-router-dom';
 import movieExplorerLogo from '../../images/logo.svg';
 import './Header.css';
 
-function Header () {
+function Header ({isLoggedIn = false}) {
   return (
-    <header className = "header">
+    <header className = {`header ${isLoggedIn ? "header_type_logged-in": "header_type_default"}`}>
        <img className="header__logo" src={movieExplorerLogo} alt="Логотип Movie Explorer"/>
-      HERE IS HEADER!
+       {
+        (isLoggedIn) ? (
+          'logged-in header'
+        ) : (
+          <nav className= "header__nav header__nav_type_default">
+            <Link to = "sign-up" className = "header__default-link header__link_type_register">Регистрация</Link>
+            <Link to = "sign-in" className = "header__default-link header__link_type_login">Войти</Link>
+          </nav>
+
+        )
+      }
     </header>
 
   );
