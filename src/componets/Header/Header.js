53bffe8a3c9dbebfed2f7e70132884e4bb7f104f
Shigatsi <shigatsii@gmail.com>
2021-03-26 23:13:51 +0300

    feat: rerendering header menu on window resize added

--- src/componets/Header/Header.js
+++ src/componets/Header/Header.js
@@ -3,13 +3,29 @@ import { Route, Link } from 'react-router-dom';
 import movieExplorerLogo from '../../images/logo.svg';
 import './Header.css';
 
-function Header ({isLoggedIn = false}) {
+function Header ({isLoggedIn = true}) {
+
+  const [width, setWidth] = React.useState(window.innerWidth)
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize)
+  })
+
+
   return (
     <header className = {`header ${isLoggedIn ? "header_type_logged-in": "header_type_default"}`}>
        <img className="header__logo" src={movieExplorerLogo} alt="Логотип Movie Explorer"/>
        {
+           (window.innerWidth >=768) ? (
+             'DESKTOP'
+           ) : ('MOBILE')
+         }
+       {
         (isLoggedIn) ? (
-          'logged-in header'
+          ' looged in nav menu'
         ) : (
           <nav className= "header__nav header__nav_type_default">
             <Link to = "sign-up" className = "header__default-link header__link_type_register">Регистрация</Link>
---
+++ src/componets/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import './Navigation.css';
+
+function Navigation () {
+  render (
+    'this is burger Menu'
+  )
+};
+
+export default Navigation;
