3621709dedf5ccc2db44212c10e1256023237686
Shigatsi <shigatsii@gmail.com>
2021-03-30 19:05:06 +0300

    feat: background layer added to sidebar

--- src/componets/Navigation/Navigation.js
+++ src/componets/Navigation/Navigation.js
@@ -20,20 +20,22 @@ function Navigation () {
         </Link>
       </div>
       <nav className = {navigationMenu ? "navigation-menu navigation-menu_type_active" : "navigation-menu" }>
-        <Link to ='#' className="navigation__bars">
-          <img className = "navigation__btn-close" src={closeBurgerIcon} onClick = {toggleNavigationMenu}/>
-        </Link>
-        <div className = "navigatio-menu__top">
-          <Link to = '/'>Главная</Link>
-          <Link to = '/movies'>Фильмы</Link>
-          <Link to = '/saved-movies'>Сохранённые фильмы</Link>
+        <div className = "navigation-menu__content">
+          <Link to ='#' className="navigation__bars">
+            <img className = "navigation__btn-close" src={closeBurgerIcon} onClick = {toggleNavigationMenu}/>
+          </Link>
+          <div className = "navigatio-menu__top">
+            <Link to = '/'>Главная</Link>
+            <Link to = '/movies'>Фильмы</Link>
+            <Link to = '/saved-movies'>Сохранённые фильмы</Link>
+          </div>
+          <Link to = "profile" className = "">
+            <figure className = "">
+              <img className=" " src={profileIcon} alt="иконкапрофиля" />
+              <figcaption className="">Аккаунт</figcaption>
+            </figure>
+          </Link>
         </div>
-        <Link to = "profile" className = "">
-          <figure className = "">
-            <img className=" " src={profileIcon} alt="иконкапрофиля" />
-            <figcaption className="">Аккаунт</figcaption>
-          </figure>
-        </Link>
       </nav>
     </>
   )
