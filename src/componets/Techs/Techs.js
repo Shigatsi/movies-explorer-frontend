97200da46221da858093bbac49e53ed8fb17d152
Shigatsi <shigatsii@gmail.com>
2021-04-02 17:04:29 +0300

    feat: techs and responsive added

--- src/componets/Techs/Techs.js
+++ src/componets/Techs/Techs.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import './Techs.css';
+
 function Techs () {
   return (
-    <div className = 'Techs'>
-     Статичный сайт
-     Адаптивный сайт
-     Одностраничное приложение
-    </div>
+    <section id="tech" className = "tech">
+     <h2 className="section-title section-title__type_tech">Технологии</h2>
+     <p className="tech__subtitle">7 технологий</p>
+     <p className="tech__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+     <ul className="tech__items">
+       <li className="tech__item">HTML</li>
+       <li className="tech__item">CSS</li>
+       <li className="tech__item">JS</li>
+       <li className="tech__item">React</li>
+       <li className="tech__item">Git</li>
+       <li className="tech__item">Express.js</li>
+       <li className="tech__item">mongoDB</li>
+     </ul>
+    </section >
   );
 }
 
